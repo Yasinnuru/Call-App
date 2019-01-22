536ca8145e55551baebf5d12ef9695593e763352
obey <yasunuru100@gmail.com>
2019-01-22 09:10:00 -0800

    Hello World

---
+++ app/src/main/java/callfromapp/yasu/MainActivity.java
@@ -0,0 +1,52 @@
+package callfromapp.yasu;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.call);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                String encodedHash = Uri.encode("#");
+                //change *999
+                String ussd = "*999" + encodedHash;
+                startActivityForResult(new Intent("android.intent.action.CALL",
+                        Uri.parse("tel:" + ussd)), 1);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Toast.makeText(getApplicationContext(),
+                "USSD: " + requestCode + "  " + resultCode + " ", Toast.LENGTH_LONG).show();
+
+        if (requestCode == 1) {
+
+            if (resultCode == RESULT_OK) {
+                // String result=data.getStringExtra("result");
+                String dd = data.toString();
+                Toast.makeText(getApplicationContext(), dd, Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+}
