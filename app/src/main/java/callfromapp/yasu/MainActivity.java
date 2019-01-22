f4b969f18388456a4a9b10917033829be1324689
Yasu info <39099655+Yasinnuru@users.noreply.github.com>
2019-01-22 09:47:18 -0800

    Update MainActivity.java

--- app/src/main/java/callfromapp/yasu/MainActivity.java
+++ app/src/main/java/callfromapp/yasu/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    //Yasuinformation @2019
         Button btn = (Button) findViewById(R.id.call);
 
         btn.setOnClickListener(new View.OnClickListener() {
